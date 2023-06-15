d6739b4b2727a7639cda982502daa2e1b6f791cf
Amanuel Belete <amanuelbelete95@gmail.com>
2023-06-15 17:29:03 +0300

    Add styling to the bookList file

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,6 +3,61 @@ import React from 'react';
 import { deleteBook } from '../redux/slices/books/bookSlice';
 
 function BookList() {
+
+  const ulStyle = {
+    padding: '0 5em',
+    overflow: 'auto',
+    maxHeight: '265px',
+  };
+
+  const liStyle = {
+    background: '#fff',
+    padding: '1em',
+    marginBottom: '1em',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  };
+
+  const catStyle = {
+    color: 'gray',
+    fontWeight: 'bold',
+    fontSize: '.9em',
+  };
+  const authorStyle = {
+    color: '#4386bf',
+  };
+  const titleStyle = {
+    fontWeight: 'bold',
+    fontSize: '1.3em',
+    marginTop: '.3em',
+    color: '#121212',
+  };
+  const editStyle = {
+    marginRight: '.8em',
+    paddingRight: '.8em',
+    borderRight: '2px solid #e9e8e8',
+    color: '#4386bf',
+    cursor: 'pointer',
+  };
+  const deleteStyle = {
+    marginRight: '.8em',
+    paddingRight: '.8em',
+    borderRight: '2px solid #e9e8e8',
+    color: '#4386bf',
+    cursor: 'pointer',
+  };
+  const commentStyle = {
+    color: '#4386bf',
+    cursor: 'pointer',
+  };
+  const div1Style = {
+    display: 'flex',
+    gap: '1em',
+  };
+  const div1pStyle = { textAlign: 'center', color: 'gray' };
+  const div1spanStyle = { fontSize: '2.2em', color: 'black' };
+
   const {
     books, isLoading, errorMsg,
   } = useSelector((store) => store.book);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
-import './index.css';
+import './index.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
