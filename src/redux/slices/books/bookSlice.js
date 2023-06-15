277330f67277072e91dc38fe0f33a7101d0ccbfc
Amanuel Belete <amanuelbelete95@gmail.com>
2023-06-15 15:44:16 +0300

    Add async function to the slice

--- src/redux/slices/books/bookSlice.js
+++ src/redux/slices/books/bookSlice.js
@@ -29,20 +29,91 @@ const initialState = {
   deleteMsg: false,
 };
 
+export const getBooks = createAsyncThunk('books/getBooks', async (_, thunkAPI) => {
+  try {
+    const resp = await axios(url);
+    return resp.data;
+  } catch (err) {
+    return thunkAPI.rejectWithValue('An error occurred while getting the data');
+  }
+});
+
+export const postBook = createAsyncThunk('postBooks/postBook', async (book, thunkAPI) => {
+  try {
+    const resp = await axios(url, {
+      method: 'POST',
+      data: JSON.stringify(book),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    if (resp.status === 201) {
+      thunkAPI.dispatch(getBooks());
+      return null;
+    }
+    return null;
+  } catch (err) {
+    return thunkAPI.rejectWithValue('Failed to post book');
+  }
+});
+
+export const deleteBook = createAsyncThunk('books/deleteBook', async (itemId, thunkAPI) => {
+  try {
+    const res = await axios(`${url}${itemId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    if (res.status === 201) {
+      thunkAPI.dispatch(getBooks());
+      return null;
+    }
+    return null;
+  } catch (err) {
+    return thunkAPI.rejectWithValue('Failed to delete the book');
+  }
+});
+
 const bookSlice = createSlice({
   name: 'book',
   initialState,
-  reducers: {
-    deleteBook: (state, action) => {
-      const bookTitle = action.payload;
-      state.books = state.books.filter((book) => book.item_id !== bookTitle);
+  extraReducers: {
+    [getBooks.pending]: (state) => {
+      state.isLoading = true;
     },
-    addBook: (state, action) => {
-      const book = action.payload;
-      state.books.push(book);
+    [getBooks.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.books = action.payload;
+    },
+    [getBooks.rejected]: (state) => {
+      state.isLoading = false;
+      state.errorMsg = true;
+    },
+    [postBook.pending]: (state) => {
+      state.postMsg = true;
+    },
+    [postBook.fulfilled]: (state, action) => {
+      state.postMsg = false;
+      state.books = action.payload;
+    },
+    [postBook.rejected]: (state) => {
+      state.postMsg = false;
+    },
+    [deleteBook.pending]: (state) => {
+      state.deleteMsg = true;
+    },
+    [deleteBook.fulfilled]: (state, action) => {
+      state.deleteMsg = false;
+      state.books = action.payload;
+    },
+    [deleteBook.rejected]: (state) => {
+      state.deleteMsg = false;
     },
   },
 });
 
-export const { addBook, deleteBook } = bookSlice.actions;
+
 export default bookSlice.reducer;
